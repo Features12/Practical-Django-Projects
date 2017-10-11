f6eb5576e6e123cca97d3f02dd64fa699a88255f
Inna_Features <features.inna@mail.ru>
2017-10-11 23:26:41 +0300

    register models

--- sample/page/admin.py
+++ sample/page/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Good, BlogArticle
 
-# Register your models here.
+admin.register(Category)
+admin.register(Good)
+admin.register(BlogArticle)
