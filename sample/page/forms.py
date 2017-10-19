d07b7312e1830b1ed4620479a9e09b624b9f7855
Inna_Features <features.inna@mail.ru>
2017-10-19 22:53:09 +0300

    add a forms

--- sample/page/forms.py
+++ sample/page/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 from .models import Good, Category, BlogArticle
+from .validaters import validate_positive
+
+
+# NAME_ERROR_LIST = {'name':{'required':'Укажите название товара',
+#                            'min_length':'Слишком короткое название',
+#                            'max_length':'Слишком длинное название'}}
 
 
 # class GoodForm(ModelForm):
@@ -10,15 +16,30 @@ from .models import Good, Category, BlogArticle
 #                   'descriptions':'Описание',
 #                   'in_stock':'Есть в наличии'}
 #         help_texts = {'name':'Должно быть уникальным'}
+#         error_messages = {'name':{'required':'Укажите название товара',
+#                                   'min_length':'Слишком короткое название',
+#                                   'max_length':'Слишком длинное название'}}
+#         widgets = {'description':forms.Textarea, 'category':forms.RadioSelect}
+#         required_css_class = 'required'
+#         error_css_class = 'error'
+
+#     def clean(self):
+#         clean_data = super(GoodForm, self).clean()
+#         if clean_data['price'] == clean_data['new_price']:
+#             raise ValueError('Цена с учетом скидки должна быть меньше!',
+#                              code = 'invalid')
+#         return clean_data
 
 
 # class GoodForm(forms.ModelForm):
 #     class Meta:
 #         model = Good
-#     name = forms.CharField(label='Название', help_text='Должно быть уникальным')
+#     name = forms.CharField(label='Название', help_text='Должно быть уникальным',
+#                            error_messages = NAME_ERROR_LIST)
 #     description = forms.CharField(widget=forms.Textarea, label='Описание')
 #     category = forms.ModelChoiceField(queryset=Category.objects.all(),
 #                                       label='Категория', empty_label=None)
+#     price = forms.FloatField(label = 'Цена', validators = [validate_positive])
 #     in_stock = forms.BooleanField(initial=True, label='Есть в наличии')
 
 
