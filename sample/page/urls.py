8ad1d24f2b940423f764e19dfb5ba8daa170378a
Inna_Features <features.inna@mail.ru>
2017-10-23 21:48:00 +0300

    add a new routers for login/logout and permissions

--- sample/page/urls.py
+++ sample/page/urls.py
@@ -6,6 +6,7 @@ from django.views.generic.dates import TodayArchiveView
 from .lwviews import (GoodListView, GoodDetailView, NewsArchiveView,\
     YearArchiveView, DayArchiveView,)
 from .CUDview import GoodDelete, GoodCreate, GoodUpdate
+from django.contrib.auth.decorators import login_required, permission_required
 
 
 urlpatterns = (
@@ -18,7 +19,7 @@ urlpatterns = (
     url(r'^(?P<year>\d{4})/(?P<month>\d+)/(?P<day>\d+)/$', DayArchiveView.as_view(), name='day_archive'),
     url(r'^today/$', TodayArchiveView.as_view(model = New, date_field='pub_date', template_name='day_archive.html'),
                                               name='today_archive'),
-    url(r'^(?P<id>\d+)/add/$', GoodCreate.as_view(), name='good_add'),
-    url(r'^good/(?P<id>\d+)/edit/$', GoodUpdate.as_view(), name='good_edit'),
-    url(r'^good/(?P<id>\d+)/delete/$', GoodDelete.as_view(), name='good_delete'),
+    url(r'^(?P<id>\d+)/add/$', login_required(GoodCreate.as_view()), name='good_add'),
+    url(r'^good/(?P<id>\d+)/edit/$', permission_required('page.change_good'), GoodUpdate.as_view(), name='good_edit'),
+    url(r'^good/(?P<id>\d+)/delete/$', permission_required('page.delete_good'), GoodDelete.as_view(), name='good_delete'),
 )
